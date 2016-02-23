2a8a0dadf2034d71b46764419f82357e897c233e
JonasMalik <jonas14550@gmail.com>
2016-02-23 22:48:42 +0100

    omdömmes stjärnor under uppbyggnad

---
+++ JavaScript/rating.js
@@ -0,0 +1,48 @@
+$("#star1").hover(function(){
+        $("#star1").css("-webkit-filter", "grayscale(0%)");
+        }, function(){
+        $("#star1").css("-webkit-filter", "grayscale(100%)");
+    });
+
+$("#star2").hover(function(){
+        $("#star1").css("-webkit-filter", "grayscale(0%)");
+        $("#star2").css("-webkit-filter", "grayscale(0%)");
+        }, function(){
+        $("#star1").css("-webkit-filter", "grayscale(100%)");
+        $("#star2").css("-webkit-filter", "grayscale(100%)");
+    });
+
+$("#star3").hover(function(){
+        $("#star1").css("-webkit-filter", "grayscale(0%)");
+        $("#star2").css("-webkit-filter", "grayscale(0%)");
+        $("#star3").css("-webkit-filter", "grayscale(0%)");
+        }, function(){
+        $("#star1").css("-webkit-filter", "grayscale(100%)");
+        $("#star2").css("-webkit-filter", "grayscale(100%)");
+        $("#star3").css("-webkit-filter", "grayscale(100%)");
+    });
+
+$("#star4").hover(function(){
+        $("#star1").css("-webkit-filter", "grayscale(0%)");
+        $("#star2").css("-webkit-filter", "grayscale(0%)");
+        $("#star3").css("-webkit-filter", "grayscale(0%)");
+        $("#star4").css("-webkit-filter", "grayscale(0%)");
+        }, function(){
+        $("#star1").css("-webkit-filter", "grayscale(100%)");
+        $("#star2").css("-webkit-filter", "grayscale(100%)");
+        $("#star3").css("-webkit-filter", "grayscale(100%)");
+        $("#star4").css("-webkit-filter", "grayscale(100%)");
+    });
+$("#star5").hover(function(){
+        $("#star1").css("-webkit-filter", "grayscale(0%)");
+        $("#star2").css("-webkit-filter", "grayscale(0%)");
+        $("#star3").css("-webkit-filter", "grayscale(0%)");
+        $("#star4").css("-webkit-filter", "grayscale(0%)");
+        $("#star5").css("-webkit-filter", "grayscale(0%)");
+        }, function(){
+        $("#star1").css("-webkit-filter", "grayscale(100%)");
+        $("#star2").css("-webkit-filter", "grayscale(100%)");
+        $("#star3").css("-webkit-filter", "grayscale(100%)");
+        $("#star4").css("-webkit-filter", "grayscale(100%)");
+        $("#star5").css("-webkit-filter", "grayscale(100%)");
+    });
