047fd857e4a0d977f1de5393b8dfdb20c952fd64
JonasMalik <jonas14550@gmail.com>
2016-02-24 23:24:56 +0100

    betygs system snart klart

--- JavaScript/rating.js
+++ JavaScript/rating.js
@@ -1,9 +1,11 @@
+var star = ["", "#star1", "#star2", "#star3", "#star4", "#star5"];
+var vote = "https://edu.oscarb.se/sjk15/api/recipe/?api_key=d7607304c8de1b93&recipe=chokladbollar&rating=";
+
 $("#star1").hover(function(){
         $("#star1").css("-webkit-filter", "grayscale(0%)");
         }, function(){
         $("#star1").css("-webkit-filter", "grayscale(100%)");
     });
-
 $("#star2").hover(function(){
         $("#star1").css("-webkit-filter", "grayscale(0%)");
         $("#star2").css("-webkit-filter", "grayscale(0%)");
@@ -11,7 +13,6 @@ $("#star2").hover(function(){
         $("#star1").css("-webkit-filter", "grayscale(100%)");
         $("#star2").css("-webkit-filter", "grayscale(100%)");
     });
-
 $("#star3").hover(function(){
         $("#star1").css("-webkit-filter", "grayscale(0%)");
         $("#star2").css("-webkit-filter", "grayscale(0%)");
@@ -21,7 +22,6 @@ $("#star3").hover(function(){
         $("#star2").css("-webkit-filter", "grayscale(100%)");
         $("#star3").css("-webkit-filter", "grayscale(100%)");
     });
-
 $("#star4").hover(function(){
         $("#star1").css("-webkit-filter", "grayscale(0%)");
         $("#star2").css("-webkit-filter", "grayscale(0%)");
@@ -46,3 +46,48 @@ $("#star5").hover(function(){
         $("#star4").css("-webkit-filter", "grayscale(100%)");
         $("#star5").css("-webkit-filter", "grayscale(100%)");
     });
+
+$('#star1').click(function() {
+   votes(1);
+});
+$('#star2').click(function() {
+   votes(2);
+});
+$('#star3').click(function() {
+   votes(3);
+});
+$('#star4').click(function() {
+   votes(4);
+});
+$('#star5').click(function() {
+   votes(5);
+});
+
+
+function votes(i){
+    $.ajax({
+        method: "GET",
+        url:  vote+""+i,
+        success: function(data) {
+            $.ajax({
+                method: "GET",
+                url: "https://edu.oscarb.se/sjk15/api/recipe/?api_key=d7607304c8de1b93&recipe=chokladbollar",
+                success: function(data) {
+                    $('#votes').text(data.votes+" röst/er");
+                    $('#rating').text(data.rating+" i snitt betyg");
+                }, 
+            });       
+        }, 
+    });
+};
+
+
+$(document).ready ( function(){
+    $.ajax({
+        method: "GET",
+        url: "https://edu.oscarb.se/sjk15/api/recipe/?api_key=d7607304c8de1b93&recipe=chokladbollar",
+        success: function(data) {
+            $('#votes').text(data.votes+" röst/er");      
+        },     
+    });
+});
